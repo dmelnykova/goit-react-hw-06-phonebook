1f3cb59ee4c611798e7e73a8aefc32630e27f67c
dariameln1 <dariamelnykova1@gmail.com>
2024-01-18 17:27:53 +0100

    completed

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Item, ButtonList } from './ContactsList.styled';
+import { deleteContact } from '../../redux/contactsSlice';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectFilter } from '../../redux/filterSlice';
+
+
+export const ContactsList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+
+  const filteredContacts = filter
+    ? contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : contacts;
+
+  const dispatch = useDispatch();
+
+  return (
+    <ul>
+      {filteredContacts.map(contact => { 
+        const { id, name, number } = contact;
+        return (
+          <Item key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <ButtonList onClick={() => dispatch(deleteContact(id))}>Delete</ButtonList>
+          </Item>
+        );
+      })}
+    </ul>
+  );
+};
